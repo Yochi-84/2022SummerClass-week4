55701720165d56ed9f3c9637c7c670964ce1e23b
Chi <yochi080684@gmail.com>
2022-07-31 00:33:05 +0800

    header & footer update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    screens: {
+      "sm": "605px",
+      "md": "1064px",
+      "lg": "1320px"
+    },
+    container: {
+      center: true,
+      padding: "12px"
+    },
+    extend: {
+      colors: {
+        "main": {
+          light: "#FBF2F2",
+          DEFAULT: "#AA0601",
+          deep: "#9F1213",
+          dark: "#650300"
+        },
+        "brown": {
+          DEFAULT: "#B75929",
+          dark: "#5F3E2D"
+        },
+        "gray": {
+          light: "#d6d6d6",
+          DEFAULT: "#DEE2E6",
+          dark: "#707070"
+        }
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
