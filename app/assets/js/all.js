f9049114ffc9fcb2a1fc9a65a4c7cb62af0da677
Chi <yochi080684@gmail.com>
2022-08-01 19:09:25 +0800

    pages update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,34 +1,35 @@
 // 漢堡選單
 document.querySelector("#hamburger-menu-icon").addEventListener("click", () => {
-  document.querySelector("#menu-list").classList.toggle("hidden");
+  document.querySelector("#menu-list").classList.toggle("max-h-65");
 });
 
 // product.html - tabs 區塊切換
-Array.from(document.querySelectorAll(".tabs-list li a")).forEach(item => item.addEventListener("click", function (e) {
+Array.from(document.querySelectorAll("#tabs-list li a")).forEach(item => item.addEventListener("click", function (e) {
   e.preventDefault();
 
-  let link = this.parentNode.getAttribute("data-link");
-  document.querySelector('.tabs-list li.is-active').classList.remove("is-active");
+  let link = this.getAttribute("data-link");
+  document.querySelector('#tabs-list a.active').classList.remove("active");
   switch (link) {
     case "optical":
-      document.querySelector("[data-link='optical']").classList.add("is-active");
+      document.querySelector("[data-link='optical']").classList.add("active");
       hideAll();
-      document.querySelector("[data-target='optical']").classList.remove("d-none");
+      document.querySelector("[data-target='optical']").classList.remove("hidden");
       break;
     case "sunglasses":
-      document.querySelector("[data-link='sunglasses']").classList.add("is-active");
+      console.log(1)
+      document.querySelector("[data-link='sunglasses']").classList.add("active");
       hideAll();
-      document.querySelector("[data-target='sunglasses']").classList.remove("d-none");
+      document.querySelector("[data-target='sunglasses']").classList.remove("hidden");
       break;
     case "functional":
-      document.querySelector("[data-link='functional']").classList.add("is-active");
+      document.querySelector("[data-link='functional']").classList.add("active");
       hideAll();
-      document.querySelector("[data-target='functional']").classList.remove("d-none");
+      document.querySelector("[data-target='functional']").classList.remove("hidden");
       break;
   }
 
   function hideAll() {
-    Array.from(document.querySelectorAll("section")).forEach(sec => sec.classList.add("d-none"));
+    Array.from(document.querySelectorAll("section")).forEach(sec => sec.classList.add("hidden"));
   }
 })
 );
@@ -36,25 +37,6 @@ Array.from(document.querySelectorAll(".tabs-list li a")).forEach(item => item.ad
 // store.html & blog.html - list & detail 切換
 const url = new URL(location.href);
 if(url.searchParams.get("id")) {
-  document.querySelector("[data-target='list']").classList.add("d-none");
-  document.querySelector("[data-target='detail']").classList.remove("d-none");
+  document.querySelector("[data-target='list']").classList.add("hidden");
+  document.querySelector("[data-target='detail']").classList.remove("hidden");
 }
-
-
-// 動態新增內文後面的 more 連結(還是直接寫 html 比較快)
-// let more_link = false;
-
-// window.addEventListener("resize", () => {
-//   if(url.pathname.endsWith("blog.html")){
-//     if(window.innerWidth <= 992 && !more_link) {
-//       Array.from(document.querySelectorAll(".news-content")).forEach(item => {
-//         let link = document.createElement("a");
-//         link.innerText = "more";
-//         link.setAttribute("href", "blog.html?id=1")
-//         link.classList.add("news-more","after-text");
-//         item.appendChild(link);
-//       });
-//       more_link = true;
-//     }
-//   }
-// })
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,21 @@ module.exports = {
       center: true,
       padding: "12px"
     },
+    fontSize: {
+      'xs':['0.75rem'],
+      'sm':['0.875rem'],
+      'base':['1rem'],
+      'lg':['1.125rem'],
+      'xl':['1.25rem'],
+      '2xl':['1.5rem'],
+      '3xl':['1.875rem'],
+      '4xl':['2.25rem'],
+      '5xl':['3rem'],
+      '6xl':['3.75rem'],
+      '7xl':['4.5rem'],
+      '8xl':['6rem'],
+      '9xl':['8rem'],
+    },
     extend: {
       colors: {
         "main": {
@@ -25,12 +40,14 @@ module.exports = {
         "gray": {
           light: "#d6d6d6",
           DEFAULT: "#DEE2E6",
-          dark: "#707070"
-        }
+          dark: "#707070",
+          text: "#495057",
+          content: "#555"
+        },
       }
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    // require('@tailwindcss/typography'),
   ],
 }
